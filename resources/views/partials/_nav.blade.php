dfa51ecac0c66837800914485bbe8dbcd0296e0a
Chris Phelan <phelanc25@gmail.com>
2017-07-28 19:09:33 -0400

    Changed links for navigation buttons. They were pointing to a previous project

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -15,9 +15,9 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="{{Request::is('/') ? "active" : ""}}"><a href="/blogTutorial/public/">Home<span class="sr-only">(current)</span></a></li>
-        <li class="{{Request::is('about') ? "active" : ""}}"><a href="/blogTutorial/public/about">About</a></li>
-        <li class="{{Request::is('contact') ? "active" : ""}}"><a href="/blogTutorial/public//contact">Contact</a></li>
+        <li class="{{Request::is('/') ? "active" : ""}}"><a href="/blog/public/">Home<span class="sr-only">(current)</span></a></li>
+        <li class="{{Request::is('about') ? "active" : ""}}"><a href="/blog/public/about">About</a></li>
+        <li class="{{Request::is('contact') ? "active" : ""}}"><a href="/blog/public/contact">Contact</a></li>
       </ul>
       
       <ul class="nav navbar-nav navbar-right">
