08592c8976ab147c18dbb67c39d7861e63d12287
Chris Phelan <phelanc25@gmail.com>
2017-07-28 09:20:31 -0400

    Modified the css route in _head.blade.php partial. Created validation rules for form in create.blade.php view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,6 +4,11 @@
 <!-- setting page title -->
 @section('title','| Create Post')
 
+<!-- importing parsley css -->
+@section('stylesheets')
+  <link rel="stylesheet" href="http://parsleyjs.org/src/parsley.css">
+@endsection
+
 <!-- displaying main content of page -->
 @section('content')
 
@@ -12,16 +17,16 @@
 			<h1>Create New Post</h1>
 			<hr>
 			<!-- creating new post entry form -->
-			<form method="POST" action="{{ route('posts.store') }}">
+			<form method="POST" action="{{ route('posts.store') }}" data-parsley-validate="">
 				<!-- creating title field -->
 				<div class="form-group">
 					<label name="title">Title:</label>
-					<input id="title" name="title" class="form-control">
+					<input id="title" name="title" class="form-control" required="" maxlength="255">
 				</div>
 				<!-- creating body field -->
 				<div class="form-group">
 					<label name="body">Post Body:</label>
-					<textarea id="body" name="body" rows="10" class="form-control"></textarea>
+					<textarea id="body" name="body" rows="10" class="form-control" required="" maxlength="255"></textarea>
 				</div>
 
 				<!-- creating button that will POST the post -->
@@ -32,3 +37,8 @@
 	</div>
 
 @endsection <!-- end of main content -->
+
+<!-- importing page specific js for validating form -->
+@section('scripts')
+    <script type="text/javascript" src="http://parsleyjs.org/dist/parsley.min.js"></script>﻿
+@endsection
